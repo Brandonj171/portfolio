438a6e9db2932b6f69ffd96b8423bafe64d0e863
Brandonj171 <Brandon.jordan14@outlook.com>
2025-06-04 12:08:47 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -159,8 +159,3 @@ particlesJS("particles-background", {
   },
   retina_detect: true
 });
-
-// Mobile navigation toggle
-document.getElementById('nav-toggle').addEventListener('click', function () {
-  document.getElementById('main-nav').classList.toggle('open');
-});
